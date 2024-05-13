2226197a03b7ac5385e29bb5f9a7ede3c8fdfc6c
jcastillo9612 <168868342+jcastillo9612@users.noreply.github.com>
2024-05-12 21:55:15 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var idsistema = 0; 
+const btnEspecialistas = document.querySelector("#");
+var result = document.getElementById("result");
+var doc1  =  document.getElementById("document1");
+var doc2  =  document.getElementById("document2");
+
+btnEspecialistas.addEventListener("click", function(){
+    result.innerHTML="<h1></h1>";
+    doc1.src="";
+    doc2.src="";
+
+});
