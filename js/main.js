66e658461e2029e5756179ca7e0585b8d1cc6ac1
jcastillo9612 <168868342+jcastillo9612@users.noreply.github.com>
2024-05-14 11:20:21 -0600

    Actualización 14 may 2024

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-var idsistema = 0; 
-const btnEspecialistas = document.querySelector("#");
-var result = document.getElementById("result");
-var doc1  =  document.getElementById("document1");
-var doc2  =  document.getElementById("document2");
+//Importaciones
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 
-btnEspecialistas.addEventListener("click", function(){
-    result.innerHTML="<h1></h1>";
-    doc1.src="";
-    doc2.src="";
 
+window.addEventListener("load", function() {
+    alert("Holaaaaa");
 });
